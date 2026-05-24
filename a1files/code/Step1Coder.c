f19b0d3df2148cca408f7eab0bd40a95ed2bc64e
LeoPaquette <leo.paquette05@gmail.com>
2026-05-24 14:09:49 -0400

    Added frog logo to top of each file. Started working on Step1coder.c. Running the .bat file gives a nonsensical output

--- a1files/code/Step1Coder.c
+++ a1files/code/Step1Coder.c
@@ -9,25 +9,26 @@
 # ECHO "=---------------------------------------="
 # ECHO "|  COMPILERS - ALGONQUIN COLLEGE (F25)  |"
 # ECHO "=---------------------------------------="
-# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    ”
-# ECHO "    @@                             @@    ”
-# ECHO "    @@           %&@@@@@@@@@@@     @@    ”
-# ECHO "    @@       @%% (@@@@@@@@@  @     @@    ”
-# ECHO "    @@      @& @   @ @       @     @@    ”
-# ECHO "    @@     @ @ %  / /   @@@@@@     @@    ”
-# ECHO "    @@      & @ @  @@              @@    ”
-# ECHO "    @@       @/ @*@ @ @   @        @@    ”
-# ECHO "    @@           @@@@  @@ @ @      @@    ”
-# ECHO "    @@            /@@    @@@ @     @@    ”
-# ECHO "    @@     @      / /     @@ @     @@    ”
-# ECHO "    @@     @ @@   /@/   @@@ @      @@    ”
-# ECHO "    @@     @@@@@@@@@@@@@@@         @@    ”
-# ECHO "    @@                             @@    ”
-# ECHO "    @@         S O F I A           @@    ”
-# ECHO "    @@                             @@    ”
-# ECHO "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    ”
+# ECHO "     -------------------------------	Â”
+# ECHO "	 |        o  o   o  o          |	Â”
+# ECHO "	 |        |\/ \^/ \/|          |	Â”
+# ECHO "	 |        |,-------.|          |	Â”
+# ECHO "	 |      ,-.(|)   (|),-.        |	Â”
+# ECHO "	 |      \_*._ ' '_.* _/        |	Â”
+# ECHO "	 |       /`-.`--' .-'\         |	Â”
+# ECHO "	 |  ,--./    `---'    \,--.    |	Â”
+# ECHO "	 |  \   |(  )     (  )|   /    |	Â”
+# ECHO "	 |   \  | ||       || |  /     |	Â”
+# ECHO "	 |    \ | /|\     /|\ | /      |	Â”
+# ECHO "	 |    /  \-._     _,-/  \      |	Â”
+# ECHO "	 |   //| \  `---'  // |\       |	Â”
+# ECHO "	 |  /,-.,-.\       /,-.,-.\    |	Â”
+# ECHO "	 |  o   o   o      o   o    o  |	Â”
+# ECHO "	 |                             |	Â”
+# ECHO "	 |  F       R       O       G  |	Â”
+# ECHO "	 -------------------------------    Â”
 # ECHO "                                         "
-# ECHO "[CODER SCRIPT ..........................]"
+# ECHO "[COMPILER SCRIPT .......................]"
 # ECHO "                                         "
 */
 
@@ -35,7 +36,7 @@
 ***********************************************************
 * File name: Reader.c
 * Compiler: MS Visual Studio 2022
-* Course: CST 8152 – Compilers, Lab Section: [011, 012, 013]
+* Course: CST 8152 Â– Compilers, Lab Section: [011, 012, 013]
 * Assignment: A12.
 * Date: Sep 01 2025
 * Professor: Paulo Sousa
@@ -63,20 +64,131 @@
 #include "Step1Coder.h"
 #endif
 
-// Function to perform the Vigenère cipher (encoding or decoding)
+// Function to perform the VigenÃ¨re cipher (encoding or decoding)
 void vigenereFile(const frog_str inputFileName, const frog_str outputFileName, const frog_str key, frog_int encode) {
-	// TO_DO: Define the input and output files (ex: FILE* inputFile, FILE* outputFile
 	// TO_DO: Use defensive programming (checking files)
+	if (inputFileName == NULL || outputFileName == NULL || key == NULL) {
+		errorPrint("Error: Null argument passed.\n");
+		return;
+	}
+
+	if (encode != CYPHER && encode != DECYPHER) {
+		errorPrint("Error: Invalid operation. Use 'CYPHER' or 'DECYPHER'.\n");
+		return;
+	}
+
 	// TO_DO: Define local variables
+	frog_int keySize = (frog_int)strlen(key);
+	frog_int keyIndex = 0;
+	frog_int inputChar;
+
+	// TO_DO: Define the input and output files (ex: FILE* inputFile, FILE* outputFile
+	FILE* inputFile = fopen(inputFileName, "r");
+	FILE* outputFile = fopen(outputFileName, "w");
+
+	if (!inputFile){
+		errorPrint("Error: Could not open input file '%s'.\n", inputFileName);
+		return;
+	}
+
+	if (!outputFile) {
+		errorPrint("Error: Could not open output file '%s'.\n", outputFileName);
+		fclose(inputFile);
+		return;
+	}
+
+	if (keySize == 0) {
+		errorPrint("Error: Key is empty.\n");
+		fclose(inputFile);
+		fclose(outputFile);
+		return;
+	}
+
 	// TO_DO: Logic: check if it is encode / decode to change the char (using Vigenere algorithm) - next function
+	while ((inputChar = fgetc(inputFile)) != EOF) {
+		if (inputChar >= ASCII_START && inputChar <= ASCII_END) {
+			frog_int keyChar = key[keyIndex % keySize] - ASCII_START;
+			frog_int outputChar;
+
+			if (encode == CYPHER) {
+				outputChar = (inputChar - ASCII_START + keyChar) % ASCII_RANGE + ASCII_START;
+			} else { // DECYPHER
+				outputChar = (inputChar - ASCII_START - keyChar + ASCII_RANGE) % ASCII_RANGE + ASCII_START;
+			}
+			fputc(outputChar, outputFile);
+			keyIndex++;
+		} else {
+			fputc(inputChar, outputFile);
+		}
+	}
+
 	// TO_DO: Close the files
+	fclose(inputFile);
+	fclose(outputFile);
 }
 
-// Function to perform the Vigenère cipher (encoding or decoding)
+// Function to perform the VigenÃ¨re cipher (encoding or decoding)
 frog_str vigenereMem(const frog_str inputFileName, const frog_str key, frog_int encode) {
 	// TO_DO define the return type and local variables
-	frog_str output = NULL;
+	frog_int keySize = (frog_int)strlen(key);
+	frog_int keyIndex = 0;
+	frog_int outputIndex = 0;
+	frog_int inputChar;
+
 	// TO_DO: Check defensive programming
+	if (inputFileName == NULL || key == NULL) {
+		errorPrint("Error: Null argument passed.\n");
+		 return NULL;
+	}
+
+	if (encode != CYPHER && encode != DECYPHER) {
+		errorPrint("Error: Invalid operation. Use 'CYPHER' or 'DECYPHER'.\n");
+		 return NULL;
+	}
+
+	frog_int fileSize = getSizeOfFile(inputFileName);
+	if (fileSize < 0) {
+		errorPrint("Error: Could not get size of input file '%s'.\n", inputFileName);
+		 return NULL;
+	}
+
+	frog_str output = (frog_str)malloc(fileSize + 1);
+	if (!output) {
+		errorPrint("Error: Could not allocate memory for output.\n");
+		 return NULL;
+	}
+
+	FILE* inputFile = fopen(inputFileName, "r");
+	if (!inputFile) {
+		errorPrint("Error: Could not open input file '%s'.\n", inputFileName);
+		free(output);
+		return NULL;
+	}
+
+	if (keySize == 0) {
+		errorPrint("Error: Key is empty.\n");
+		fclose(inputFile);
+		free(output);
+		return NULL;
+	}
+
+	while (inputChar = fgetc(inputFile) != EOF && outputIndex < fileSize ) {
+		if (output[outputIndex] >= ASCII_START && output[outputIndex] <= ASCII_END) {
+			frog_int keyChar = key[keyIndex % keySize] - ASCII_START;
+			frog_int outputChar;
+
+			if (encode == CYPHER) {
+				outputChar = (output[outputIndex] - ASCII_START + keyChar) % ASCII_RANGE + ASCII_START;
+			} else { // DECYPHER
+				outputChar = (output[outputIndex] - ASCII_START - keyChar + ASCII_RANGE) % ASCII_RANGE + ASCII_START;
+			}
+			output[outputIndex++] = (frog_char)outputChar;
+			keyIndex++;
+		} else {
+			output[outputIndex++] = (frog_char)inputChar;
+		}
+	}
+
 	// TO_DO: Use the logic to code/decode - consider the logic about visible chars only
 	return output;
 }
@@ -92,8 +204,36 @@ void decypher(const frog_str inputFileName, const frog_str outputFileName, const
 }
 
 // TO_DO: Get file size (util method)
-frog_int getSizeOfFile(const frog_int filename) {
-	frog_int size = 0;
+frog_int getSizeOfFile(const frog_str filename) {
+
     // TO_DO: Use the logic to get the size of the file
+	if (filename == NULL) {
+		errorPrint("Error: Null argument passed.\n");
+		return -1;
+	}
+
+	FILE* file = fopen(filename, "r");
+	frog_int size = 0;
+
+	if (!file) {
+		errorPrint("Error: Could not open file '%s'.\n", filename);
+		return -1;
+	}
+
+	 if (fseek(file, 0, SEEK_END) != 0) {
+	 	errorPrint("Error: Could not seek file '%s'.\n", filename);
+	 	fclose(file);
+	 	return -1;
+	 };
+
+	size = (frog_int)ftell(file);
+	if (size < 0) {
+		errorPrint("Error: Could not tell file position for '%s'.\n", filename);
+		fclose(file);
+		return -1;
+	}
+
+	fclose(file);
+
     return size;
 }
