5eaf8551ed6352e157b8eeeec7ef34e47da5fd24
Cujoca <andrei@razie.com>
2026-05-31 17:52:26 -0400

    Moved all files related to Assignment01 into its own designated folder.

---
+++ Assignment01/a1files/code/Compilers.h
@@ -0,0 +1,135 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Spring, 2026
+* Author: Andrei Cojocaru and Leo Paquette
+* Professor: Vardaan Sangar
+************************************************************
+#
+# ECHO "=---------------------------------------="
+# ECHO "|  COMPILERS - ALGONQUIN COLLEGE (F25)  |"
+# ECHO "=---------------------------------------="
+# ECHO "     -------------------------------	”
+# ECHO "	 |        o  o   o  o          |	”
+# ECHO "	 |        |\/ \^/ \/|          |	”
+# ECHO "	 |        |,-------.|          |	”
+# ECHO "	 |      ,-.(|)   (|),-.        |	”
+# ECHO "	 |      \_*._ ' '_.* _/        |	”
+# ECHO "	 |       /`-.`--' .-'\         |	”
+# ECHO "	 |  ,--./    `---'    \,--.    |	”
+# ECHO "	 |  \   |(  )     (  )|   /    |	”
+# ECHO "	 |   \  | ||       || |  /     |	”
+# ECHO "	 |    \ | /|\     /|\ | /      |	”
+# ECHO "	 |    /  \-._     _,-/  \      |	”
+# ECHO "	 |   //| \  `---'  // |\       |	”
+# ECHO "	 |  /,-.,-.\       /,-.,-.\    |	”
+# ECHO "	 |  o   o   o      o   o    o  |	”
+# ECHO "	 |                             |	”
+# ECHO "	 |  F       R       O       G  |	”
+# ECHO "	 -------------------------------    ”
+# ECHO "                                         "
+# ECHO "[COMPILER SCRIPT .......................]"
+# ECHO "                                         "
+/*
+
+************************************************************
+* File name: Compilers.h
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 Compilers, Lab Section: 301
+* Assignment: A1
+* Date: May 31, 2026
+* Professor: Vardaan Sangar
+* Purpose: This file defines the functions called by main function.
+* Function list: mainReader(), mainScanner(), mainParser().
+*************************************************************/
+
+#ifndef COMPILERS_H_
+#define COMPILERS_H_
+
+#define DEBUG 0
+
+/*
+ * ............................................................................
+ * ADVICE 1:
+ * Please check the "TODO" labels to develop your activity.
+ *
+ * ADVICE 2: This file must be updated according to each assignment
+ * (see the progression: reader > scanner > parser).
+ * ............................................................................
+ */
+
+/* TO_DO: Adjust your language (cannot be "Sofia") */
+/* Language name */
+#define STR_LANGNAME	"Frog"
+
+/* Logical constants - adapt for your language */
+#define FROG  1
+#define FROG_FALSE 0
+
+#define INVALID NULL
+#define EOF_CHAR '\0'
+
+/*
+------------------------------------------------------------
+Data types definitions
+NOTE: Some types may not be directly used by your language,
+		but they can be necessary for conversions.
+------------------------------------------------------------
+*/
+
+/* TO_DO: Define your typedefs */
+typedef char			frog_char;
+typedef char*			frog_str;
+typedef int				frog_int;
+typedef float			frog_float;
+typedef void			frog_void;
+
+typedef unsigned char	frog_bool;
+typedef unsigned char	frog_byte;
+
+typedef long			frog_long;
+typedef double			frog_doub;
+
+/*
+------------------------------------------------------------
+Programs:
+1: Coder - invokes Main1Coder code
+2: Reader - invokes Main2Reader code
+3: Scanner - invokes Main3Scanner code
+4: Parser - invokes Main4Parser code
+5: Writer - invokes Main5Writer code
+------------------------------------------------------------
+*/
+enum PROGRAMS {
+	PGM_CDR = '1', // Coder:	05pt
+	PGM_RDR = '2', // Reader:	05pt
+	PGM_SCN = '3', // Scanner:	15pt
+	PGM_PSR = '4', // Parser:	15pt
+	PGM_WRT = '5', // Writer:	10pt
+};
+
+/*
+------------------------------------------------------------
+Main functions signatures
+(Code will be updated during next assignments)
+------------------------------------------------------------
+*/
+frog_int main1Coder	(frog_int argc, frog_str* argv);
+
+frog_void printLogo();
+frog_void errorPrint(frog_str fmt, ...);
+
+#endif
+
+/*
+	  "args": [
+		1,
+		1,
+		"README.txt",
+		"CODED.txt"
+	  ]
+	  "args": [
+		4,
+		"CODED.txt"
+	  ]
+*/
---
+++ Assignment01/a1files/code/Step1Coder.c
@@ -0,0 +1,258 @@
+/*
+************************************************************
+* COMPILERS COURSE - Algonquin College
+* Code version: Spring, 2026
+* Author: Andrei Cojocaru and Leo Paquette
+* Professor: Vardaan Sangar
+************************************************************
+#
+# ECHO "=---------------------------------------="
+# ECHO "|  COMPILERS - ALGONQUIN COLLEGE (F25)  |"
+# ECHO "=---------------------------------------="
+# ECHO "     -------------------------------	
+# ECHO "	 |        o  o   o  o          |	
+# ECHO "	 |        |\/ \^/ \/|          |	
+# ECHO "	 |        |,-------.|          |	
+# ECHO "	 |      ,-.(|)   (|),-.        |	
+# ECHO "	 |      \_*._ ' '_.* _/        |	
+# ECHO "	 |       /`-.`--' .-'\         |	
+# ECHO "	 |  ,--./    `---'    \,--.    |	
+# ECHO "	 |  \   |(  )     (  )|   /    |	
+# ECHO "	 |   \  | ||       || |  /     |	
+# ECHO "	 |    \ | /|\     /|\ | /      |	
+# ECHO "	 |    /  \-._     _,-/  \      |	
+# ECHO "	 |   //| \  `---'  // |\       |	
+# ECHO "	 |  /,-.,-.\       /,-.,-.\    |	
+# ECHO "	 |  o   o   o      o   o    o  |	
+# ECHO "	 |                             |	
+# ECHO "	 |  F       R       O       G  |	
+# ECHO "	 -------------------------------    
+# ECHO "                                         "
+# ECHO "[COMPILER SCRIPT .......................]"
+# ECHO "                                         "
+*/
+
+/*
+***********************************************************
+* File name: Reader.c
+* Compiler: MS Visual Studio 2022
+* Course: CST 8152 Compilers, Lab Section: 301
+* Assignment: A1
+* Date: May 31, 2026
+* Professor: Vardaan Sangar
+* Purpose: This file is the main code for Buffer/Reader (A12)
+************************************************************
+*/
+
+/*
+ *.............................................................................
+ * MAIN ADVICE:
+ * - Please check the "TODO" labels to develop your activity.
+ * - Review the functions to use "Defensive Programming".
+ *.............................................................................
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef COMPILERS_H_
+#include "Compilers.h"
+#endif
+
+#ifndef CODER_H_
+#include "Step1Coder.h"
+#endif
+
+// Function to perform the Vigenère cipher (encoding or decoding)
+void vigenereFile(const frog_str inputFileName, const frog_str outputFileName, const frog_str key, frog_int encode) {
+	// Checking for null arguments
+	if (inputFileName == NULL || outputFileName == NULL || key == NULL) {
+		errorPrint("Error: Null argument passed.\n");
+		return;
+	}
+
+	// Checking for valid encoding option
+	if (encode != CYPHER && encode != DECYPHER) {
+		errorPrint("Error: Invalid operation. Use 'CYPHER' or 'DECYPHER'.\n");
+		return;
+	}
+
+	// Local variables
+	frog_int keySize = (frog_int)strlen(key);
+	frog_int keyIndex = 0;
+	frog_int inputChar;
+
+	// Input and output files
+	FILE* inputFile = fopen(inputFileName, "r");
+	FILE* outputFile = fopen(outputFileName, "w");
+
+	// Checking if files were opened successfully
+	if (!inputFile){
+		errorPrint("Error: Could not open input file '%s'.\n", inputFileName);
+		return;
+	}
+
+	if (!outputFile) {
+		errorPrint("Error: Could not open output file '%s'.\n", outputFileName);
+		fclose(inputFile);
+		return;
+	}
+
+	// Checking for key length
+	if (keySize == 0) {
+		errorPrint("Error: Key is empty.\n");
+		fclose(inputFile);
+		fclose(outputFile);
+		return;
+	}
+
+	// Check if it is encode/decode to change the char (using Vigenere algorithm)
+	while ((inputChar = fgetc(inputFile)) != EOF) {
+		if (inputChar >= ASCII_START && inputChar <= ASCII_END) {
+			frog_int keyChar = key[keyIndex % keySize] - ASCII_START;
+			frog_int outputChar;
+
+			if (encode == CYPHER) {
+				outputChar = (inputChar - ASCII_START + keyChar) % ASCII_RANGE + ASCII_START;
+			} else { // DECYPHER
+				outputChar = (inputChar - ASCII_START - keyChar + ASCII_RANGE) % ASCII_RANGE + ASCII_START;
+			}
+			fputc(outputChar, outputFile);
+			keyIndex++;
+		} else {
+			fputc(inputChar, outputFile);
+		}
+	}
+
+	// Closing the files
+	fclose(inputFile);
+	fclose(outputFile);
+}
+
+// Function to perform the Vigenère cipher (encoding or decoding)
+frog_str vigenereMem(const frog_str inputFileName, const frog_str key, frog_int encode) {
+	// Checking for null arguments
+	if (inputFileName == NULL || key == NULL) {
+		errorPrint("Error: Null argument passed.\n");
+		 return NULL;
+	}
+
+	// Checking for valid encoding option
+	if (encode != CYPHER && encode != DECYPHER) {
+		errorPrint("Error: Invalid operation. Use 'CYPHER' or 'DECYPHER'.\n");
+		 return NULL;
+	}
+
+	// Local Variables
+	frog_int keySize = (frog_int)strlen(key);
+	frog_int keyIndex = 0;
+	frog_int outputIndex = 0;
+	frog_int inputChar;
+
+	// Get size of file and allocate memory for output
+	frog_int fileSize = getSizeOfFile(inputFileName);
+	if (fileSize < 0) {
+		errorPrint("Error: Could not get size of input file '%s'.\n", inputFileName);
+		 return NULL;
+	}
+
+	frog_str output = (frog_str)malloc(fileSize + 1);
+	if (!output) {
+		errorPrint("Error: Could not allocate memory for output.\n");
+		 return NULL;
+	}
+
+	// Opening input file
+	FILE* inputFile = fopen(inputFileName, "r");
+	if (!inputFile) {
+		errorPrint("Error: Could not open input file '%s'.\n", inputFileName);
+		free(output);
+		return NULL;
+	}
+
+	// Checking for key length
+	if (keySize == 0) {
+		errorPrint("Error: Key is empty.\n");
+		fclose(inputFile);
+		free(output);
+		return NULL;
+	}
+
+<<<<<<< HEAD
+	while ((inputChar = fgetc(inputFile)) != EOF && outputIndex < fileSize ) {
+		if (output[outputIndex] >= ASCII_START && output[outputIndex] <= ASCII_END) {
+=======
+	// Logic to encode/decode for only visible chars
+	while ((inputChar = fgetc(inputFile)) != EOF && outputIndex < fileSize ) {
+		if (inputChar >= ASCII_START && inputChar <= ASCII_END) {
+>>>>>>> refs/remotes/origin/Leo
+			frog_int keyChar = key[keyIndex % keySize] - ASCII_START;
+			frog_int outputChar;
+
+			if (encode == CYPHER) {
+				outputChar = (inputChar - ASCII_START + keyChar) % ASCII_RANGE + ASCII_START;
+			} else { // DECYPHER
+				outputChar = (inputChar - ASCII_START - keyChar + ASCII_RANGE) % ASCII_RANGE + ASCII_START;
+			}
+			output[outputIndex++] = (frog_char)outputChar;
+			keyIndex++;
+		} else {
+			output[outputIndex++] = (frog_char)inputChar;
+		}
+	}
+
+	// Null terminate the string and close the file
+	output[outputIndex] = '\0';
+	fclose(inputFile);
+	return output;
+}
+
+// Function to encode (cypher)
+void cypher(const frog_str inputFileName, const frog_str outputFileName, const frog_str key) {
+    vigenereFile(inputFileName, outputFileName, key, CYPHER);
+}
+
+// Function to decode (decypher)
+void decypher(const frog_str inputFileName, const frog_str outputFileName, const frog_str key) {
+    vigenereFile(inputFileName, outputFileName, key, DECYPHER);
+}
+
+// TO_DO: Get file size (util method)
+frog_int getSizeOfFile(const frog_str filename) {
+
+    // Checking for null parameter
+	if (filename == NULL) {
+		errorPrint("Error: Null argument passed.\n");
+		return -1;
+	}
+
+	FILE* file = fopen(filename, "r");
+	frog_int size = 0;
+
+	// Checking if the file opened successfully
+	if (!file) {
+		errorPrint("Error: Could not open file '%s'.\n", filename);
+		return -1;
+	}
+
+	// Logic to get the size of the file
+	 if (fseek(file, 0, SEEK_END) != 0) {
+	 	errorPrint("Error: Could not seek file '%s'.\n", filename);
+	 	fclose(file);
+	 	return -1;
+	 };
+
+	// Logic to get the current position
+	size = (frog_int)ftell(file);
+	if (size < 0) {
+		errorPrint("Error: Could not tell file position for '%s'.\n", filename);
+		fclose(file);
+		return -1;
+	}
+
+	// Closing the file
+	fclose(file);
+
+    return size;
+}
