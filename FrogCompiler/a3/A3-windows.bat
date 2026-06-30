5265cb1eef2f8daf8c8f60a0ce8369f6c672cf92
LeoPaquette <leo.paquette05@gmail.com>
2026-06-30 14:33:47 -0400

    Added A3 files with updated headers and adjusted data types

---
+++ FrogCompiler/a3/A3-windows.bat
@@ -0,0 +1,32 @@
+:: A3 SCRIPT
+echo off
+echo '--------------------------------------------'
+echo '-      ALGONQUIN COLLEGE - COM - 25F       -'
+echo '--------------------------------------------'
+echo '-                                          -'
+echo '-    ====                                  -'
+echo '-   =                                      -'
+echo '-  =       ==   = =  ==  = =   =   ==  ==  -'
+echo '-   =     =  = = = = = = = =   ==  =  ==   -'
+echo '-    ====  ==  = = = =   = === === =   ==  -'
+echo '-                                          -'
+echo '--------------------------------------------'
+echo '-  [A3: Scanner - Team: Paulo Sousa / God] -'
+echo '--------------------------------------------'
+set "CODE_DIR=%~dp0..\code"
+set "INPUT_DIR=%~dp0..\input"
+set "arg=%1"
+set "param=2"
+if "%arg%"=="" set "arg=CODED.txt"
+:: del "%INPUT_DIR%\CODED.txt" "%INPUT_DIR%\RESTORED.txt"
+
+gcc "%CODE_DIR%\compilers.c" "%CODE_DIR%\main1coder.c" "%CODE_DIR%\step1coder.c" "%CODE_DIR%\main2reader.c" "%CODE_DIR%\step2reader.c" "%CODE_DIR%\main3scanner.c" "%CODE_DIR%\step3scanner.c" -o "%CODE_DIR%\compilers"
+ping -n 2 127.0.0.1 >nul
+"%CODE_DIR%\compilers" 1 1 "%INPUT_DIR%\README.txt" "%arg%"
+ping -n 2 127.0.0.1 >nul
+
+"%CODE_DIR%\compilers" 3 "%arg%" > "%~dp0..\output\out.txt" 2> "%~dp0..\output\err.txt"
+ping -n 2 127.0.0.1 >nul
+
+dir "%~dp0..\output\out.txt" "%~dp0..\output\err.txt"
+type "%~dp0..\output\out.txt"
